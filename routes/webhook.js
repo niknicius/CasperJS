4492f9407b312883d5d2d7ccbad156e94165c15f
Vinnicius S <niknicius@gmail.com>
2019-08-18 20:54:36 -0300

    Bug Noticias Vazias arrumado

--- routes/webhook.js
+++ routes/webhook.js
@@ -78,7 +78,7 @@ function handleMessage(sender_psid, received_message){
 
     if(received_message.quick_reply){
         response = parse_msg(received_message.quick_reply.payload);
-        if(response === false){
+        if(response === false || response === undefined){
             response = "Desculpe-me! Não existem notícias cadastradas para esse tema!";
             console.log(response);
             callSendApi(sender_psid, response);
