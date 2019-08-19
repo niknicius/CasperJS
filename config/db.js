a9f23f2b8c27dd4900abc36b46dc3d73178e497d
Vinnicius S <niknicius@gmail.com>
2019-08-19 09:31:52 -0300

    Banco migrado para Umbler

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://niknicius01:preguicaPass15@mongodb.niknicius.kinghost.net/niknicius01', {
+mongoose.connect('mongodb://niknicius:t4C7*iVF*d6C@kamino.mongodb.umbler.com:41971/casper', {
     useNewUrlParser: true,
 });
 
